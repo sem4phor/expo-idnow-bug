f20fd10710abecc28f812d843df877424c614e24
vrapp <valentin.rapp@justtrade.com>
2023-03-28 12:24:06 +0200

    Add idnow code

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { IdnowManager } from '@tokenstreet/react-native-idnow-videoident'
 
 export default function App() {
+
+  const handlePress = async () => {
+    try {
+      console.log('START IDENT')
+      const { resultCode } = await IdnowManager.startVideoIdent({ transactionToken: 'TST-YOURCODE' });
+      console.log(resultCode);
+    } catch (error) {
+      console.error(error)
+      if (error !== null && typeof error === 'object' && 'resultCode' in error) {
+          const identificationError = error;
+          console.log(identificationError.resultCode);
+          console.log(identificationError.errorMessage);
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Button title="Start VideoIdent" onPress={handlePress}>
+        </Button>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
